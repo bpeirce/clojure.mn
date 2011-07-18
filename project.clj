1d27b42067ea615012d04a3dd6e0b02b25f942be
Ben Peirce <bpeirce@gmail.com>
2011-07-18 17:06:02 -0500

    Create basic app.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject clojuremn "1.0.0-SNAPSHOT"
   :description "The web site for Clojure dot MN, the Minnesota Clojure group."
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [ring/ring-core "0.3.8"]
+                 [ring/ring-jetty-adapter "0.3.8"]]
+  :exclusions [org.mortbay.jetty/servlet-api])
--- src/clojuremn/core.clj
+++ src/clojuremn/core.clj
@@ -1,1 +1,10 @@
-(ns clojuremn.core)
+(ns clojuremn.core
+  (:use ring.util.response
+        ring.adapter.jetty))
+
+(defn app [req]
+  (response "Hello World"))
+
+(defn -main []
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
+    (run-jetty app {:port port})))
