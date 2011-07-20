538a8cb497dcdd7662fbdbb41f6c5ff382afaab5
Ben Peirce <bpeirce@gmail.com>
2011-07-20 01:59:39 -0500

    Update home page and styles.

--- src/clojuremn/homepage.clj
+++ src/clojuremn/homepage.clj
@@ -14,6 +14,15 @@
      [:h1 "Clo"
       [:span {:id "clojure-j"} "j"]
       "ure.mn"]
-     [:p {:id "headline"} "The Minnesota Clojure Users Group"]
-     [:p "this is some content"]]
+     [:p {:id "tagline"} "The Minnesota Clojure Users Group"]
+     [:p {:class "announcement"} "Join our "
+      [:a {:href "http://groups.google.com/group/clojuremn"} "mailing list"] "."]
+     [:h2 "Next Meeting"]
+     [:p "Our next meeting will be on Wednesday, August 10th at 7pm at the "
+      [:a {:href "http://maps.google.com/maps/place?q=dunn+bros&cid=14517305764480210858"} "Dunn Bros on 3rd Avenue downtown"] "."]
+     [:h2 "July 13, 2011"]
+     [:p "We discussed "
+      [:a {:href "http://clojure-conj.org"} "Clojure Conj 2011"]
+      ", compared the JVM to the CLR, and surveyed the NoSQL landscape."]
+     ]
     ]))
