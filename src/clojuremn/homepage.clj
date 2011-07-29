7d789b202af095aeda34e94232b776b8cb6ad0ea
Ben Peirce <bpeirce@gmail.com>
2011-07-29 16:46:01 -0500

    Add some homepage updates.

--- src/clojuremn/homepage.clj
+++ src/clojuremn/homepage.clj
@@ -11,15 +11,20 @@
     (include-css "/stylesheets/base.css")]
    [:body
     [:div {:id "content"}
-     [:h1 "Clo"
-      [:span {:id "clojure-j"} "j"]
-      "ure.mn"]
+     [:h1 "Clo" [:span {:id "clojure-j"} "j"] "ure.mn"]
      [:p {:id "tagline"} "The Minnesota Clojure Users Group"]
-     [:p {:class "announcement"} "Join our "
-      [:a {:href "http://groups.google.com/group/clojuremn"} "mailing list"] "."]
-     [:h2 "Next Meeting"]
+     [:p {:class "announcement"}
+      "Join our "
+      [:a {:href "http://groups.google.com/group/clojuremn"} "mailing list"]
+      "."
+      ]
+     [:p {:class "announcement"}
+      "Follow us on "
+      [:a {:href "http://twitter.com/clojuremn"} "Twitter"]
+      "."
+      ]     [:h2 "Next Meeting"]
      [:p "Our next meeting will be on Wednesday, August 10th at 7pm at the "
-      [:a {:href "http://maps.google.com/maps/place?q=dunn+bros&cid=14517305764480210858"} "Dunn Bros on 3rd Avenue downtown"] "."]
+      [:a {:href "http://freighthouse.dunnbros.com/"} "Freight House Dunn Bros"] "."]
      [:h2 "July 13, 2011"]
      [:p "We discussed "
       [:a {:href "http://clojure-conj.org"} "Clojure/conj 2011"]
