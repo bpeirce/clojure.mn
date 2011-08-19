4b8650fab7c3d371722b5fe1d2cb771c053ca815
Ben Peirce <bpeirce@gmail.com>
2011-08-19 15:30:51 -0500

    Add next meeting location and notes from last meeting.

--- src/clojuremn/homepage.clj
+++ src/clojuremn/homepage.clj
@@ -16,17 +16,31 @@
      [:p {:class "announcement"}
       "Join our "
       [:a {:href "http://groups.google.com/group/clojuremn"} "mailing list"]
-      "."
-      ]
-     [:p {:class "announcement"}
-      "Follow us on "
+      ". Follow us on "
       [:a {:href "http://twitter.com/clojuremn"} "Twitter"]
       "."
       ]
      [:img {:id "logo" :src "/images/lambda.png"}]
      [:h2 "Next Meeting"]
-     [:p "Our next meeting will be on Wednesday, August 10th at 7pm at the "
-      [:a {:href "http://freighthouse.dunnbros.com/"} "Freight House Dunn Bros"] "."]
+     [:p "Our next meeting will be on Wednesday, September 7th at 7pm at "
+      [:a {:href "http://refactr.com/contact/"} "Refactr"] "."]
+
+     [:h2 "August 10, 2011"]
+     [:p 
+      [:a {:href "http://twitter.com/bpeirce"} "Ben"]
+      " walked through how he used "
+      [:a {:href "https://github.com/mmcgrana/ring"} "Ring"]
+      " and "
+      [:a {:href "https://github.com/weavejester/hiccup"} "Hiccup"]
+      " to make "
+      [:a {:href "https://github.com/bpeirce/clojure.mn"} "this site"]
+      ".  "
+      [:a {:href "http://twitter.com/bmaddy"} "Brian"]
+      " showed how he used "
+      [:a {:href "https://github.com/cgrand/enlive"} "Enlive"]
+      " for one of his sites."
+      ]
+      
      [:h2 "July 13, 2011"]
      [:p "We discussed "
       [:a {:href "http://clojure-conj.org"} "Clojure/conj 2011"]
